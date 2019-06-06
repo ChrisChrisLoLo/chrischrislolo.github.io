384ff6f17602f574c3fe6fa7e1b2a3d8111c3450
ChrisChrisLoLo <sporewoh@gmail.com>
2019-06-06 00:22:13 -0600

    Updates

---
+++ precache-manifest.a3bfaab87742156530a37c1cb3245649.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f83e511c248856e20c45014f0a62056",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de23b7db1f4a74fe1591",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "5fb2261ca1f48c7a8632",
+    "url": "/static/css/main.595abe7d.chunk.css"
+  },
+  {
+    "revision": "de23b7db1f4a74fe1591",
+    "url": "/static/js/2.4749e509.chunk.js"
+  },
+  {
+    "revision": "5fb2261ca1f48c7a8632",
+    "url": "/static/js/main.5a21cbbe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a945e69f9db52580dcb3b9d4c719773b",
+    "url": "/static/media/demo0.a945e69f.gif"
+  },
+  {
+    "revision": "85b709891c2fb3cd5f560e48238d023b",
+    "url": "/static/media/demo1.85b70989.gif"
+  },
+  {
+    "revision": "2e669e7eed9c1f859e077fefa8082235",
+    "url": "/static/media/demo2.2e669e7e.gif"
+  },
+  {
+    "revision": "f95119f9d2baa8e5769ecdc181314360",
+    "url": "/static/media/demo3.f95119f9.gif"
+  },
+  {
+    "revision": "5817db4cd377a6a19cab281fde92a305",
+    "url": "/static/media/demo5.5817db4c.gif"
+  },
+  {
+    "revision": "a1226aff1b0dfa6ee16583d8fd4c707e",
+    "url": "/static/media/hero.a1226aff.jpg"
+  },
+  {
+    "revision": "93aa1ffee9579cffd90b77ef30b2cf80",
+    "url": "/static/media/modeling.93aa1ffe.svg"
+  },
+  {
+    "revision": "d605f8e48a5822b8170cbfc0b0a30c80",
+    "url": "/static/media/prof.d605f8e4.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.a3bfaab87742156530a37c1cb3245649.js"
 );
 
 self.addEventListener('message', (event) => {
